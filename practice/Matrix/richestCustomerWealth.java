2db5034dc8a40f21595bec3a091e4168c7b98b92
amitvapal <amitvapal@gmail.com>
2024-05-12 12:36:31 -0400

    array file richest customer

---
+++ practice/Matrix/richestCustomerWealth.java
@@ -0,0 +1,31 @@
+package practice.Matrix;
+
+import java.util.*;
+
+public class richestCustomerWealth {
+    public static int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;  // Initialize maxWealth to 0
+        // ToDo: Write Your Code Here.
+        int max = Arrays.stream(accounts[0]).sum();
+        for(int i = 1; i < accounts.length; i++){
+            if(Arrays.stream(accounts[i]).sum() > max){
+                max = Arrays.stream(accounts[i]).sum();
+            }
+
+            
+            
+        }
+        maxWealth = max;
+
+        return maxWealth;
+    }
+
+    public static void main(String[] args) {
+        richestCustomerWealth sol = new richestCustomerWealth();
+        // Example test cases
+        System.out.println(sol.maximumWealth(new int[][]{{5,2,3},{0,6,7}}));  // 13
+        System.out.println(sol.maximumWealth(new int[][]{{1,2},{3,4},{5,6}}));  // 11
+        System.out.println(sol.maximumWealth(new int[][]{{10,100},{200,20},{30,300}}));  // 330
+    }
+}
+
